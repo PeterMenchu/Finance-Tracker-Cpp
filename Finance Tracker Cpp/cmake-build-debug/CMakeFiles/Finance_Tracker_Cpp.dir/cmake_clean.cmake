c2458a32dbc70d6a7bf16f60b3277243f2db12d6
Peter Menchu <menchupeter2@gmail.com>
2020-03-08 05:40:16 -0500

    Pushed all project files to reduce commit notifications.

---
+++ Finance Tracker Cpp/cmake-build-debug/CMakeFiles/Finance_Tracker_Cpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Finance_Tracker_Cpp.dir/Finance.cpp.o"
+  "CMakeFiles/Finance_Tracker_Cpp.dir/main.cpp.o"
+  "Finance_Tracker_Cpp"
+  "Finance_Tracker_Cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Finance_Tracker_Cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
